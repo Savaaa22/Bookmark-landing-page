070a7cfe446234a8c42509178e773850f485bb34
Sava Polimac <savaapolimac@gmail.com>
2020-02-24 17:56:37 +0100

    mobile nav-menu style

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const hamburger = document.querySelector(".icon-hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const closeBtn = document.querySelector(".icon-close");
+const logoBookmark = document.querySelector(".logo-bookmark");
 
 hamburger.addEventListener("click", function() {
   navMenu.style.height = "100%";
+  hamburger.classList.add("hidden");
+  logoBookmark.classList.add("hidden");
 });
 
 closeBtn.addEventListener("click", function() {
   navMenu.style.height = "0%";
+  hamburger.classList.remove("hidden");
+  logoBookmark.classList.remove("hidden");
 });
